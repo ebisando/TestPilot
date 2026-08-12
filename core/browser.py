c433c15702437281e9740de6a2aa18feb57f787b
ebisando <abbylee.ca@gmail.com>
2026-08-12 17:01:37 -0400

    fix: update browser configuration for github actions

--- core/browser.py
+++ core/browser.py
@@ -1,6 +1,4 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 
 
@@ -10,12 +8,13 @@ def create_driver(headless=False):
 
     if headless:
         options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--window-size=1920,1080")
 
-    options.add_argument("--start-maximized")
+    if not headless:
+        options.add_argument("--start-maximized")
 
-    driver = webdriver.Chrome(
-        service=Service(ChromeDriverManager().install()),
-        options=options
-    )
+    driver = webdriver.Chrome(options=options)
 
     return driver
